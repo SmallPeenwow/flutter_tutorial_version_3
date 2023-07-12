02f828217649d139dea35a1ce2749098cc917281
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-12 11:09:25 +0200

    Colors Done

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: const RootPage(),
     );
   }
